ccd93634d913c36b879648af166f32371f7c8dcb
Anton Bukov <k06aaa@gmail.com>
2021-09-28 08:49:09 +0200

    Fix calldatacopy to returndatacopy

--- contracts/ERC3652PureProxy.sol
+++ contracts/ERC3652PureProxy.sol
@@ -8,9 +8,9 @@ contract ERC3652PureProxy {
         assembly { // solhint-disable-line no-inline-assembly
             mstore(0, 0xc438959200000000000000000000000000000000000000000000000000000000)
             if staticcall(gas(), caller(), 0, 4, 0, 0) {
-                calldatacopy(0, 0, calldatasize())
-                if delegatecall(gas(), shr(96, mload(52)), 72, sub(mload(0), 40), 0, 0) {
-                    selfdestruct(shr(96, mload(32)))
+                returndatacopy(0, 0, returndatasize())
+                if delegatecall(gas(), shr(96, mload(20)), 40, sub(returndatasize(), 40), 0, 0) {
+                    selfdestruct(shr(96, mload(0)))
                 }
             }
             revert(0, 0)
